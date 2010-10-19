ff5e7b9cf65b24bbd61eea110eee502f86a92a7f
Ruprict <glenn.goodrich@gmail.com>
2010-10-19 11:02:43 -0400

    fix up templates for IE

--- jquery.agsLegend.templates.js
+++ jquery.agsLegend.templates.js
@@ -1,6 +1,7 @@
-	<script type="text/x-jquery-tmpl" id="layerTemplate" >
-			<li><input type="checkbox" style="float:left" id="${$item.getCheckboxID()}" checked="${$item.getChecked()}" />
-			<h3 style="margin:0 0 0 25px;padding:0;" >${name}<img src="jQuery.agsLegend/collapsible_arrow.png"/></h3>
+<script type="text/x-jquery-tmpl" id="layerTemplate" >
+		<li style="float:left">
+			<input type="checkbox" style="float:left" id="${$item.getCheckboxID()}" checked="${$item.getChecked()}" />
+			<h3 class="agsLegendOpen" style="margin:0 0 0 25px;padding:0;" >${name}</h3>
 			{{if drawingInfo.renderer.type=="simple"}}
 				
 				{{if drawingInfo.renderer.symbol.type=='esriPMS'}}
@@ -9,10 +10,10 @@
 					<span style="padding:0px 5px 0px 5px;display:table-cell;vertical-align:middle;">${name}</span>
 					</div>
 				{{else drawingInfo.renderer.symbol.type=='esriSFS'}}
-					<div style="display:table;" >
+					<div style="display:table;padding:10px 0px 0px 45px;" >
 					
-					<div style="display:table-cell;width:25px;height:25px;background-color:${$item.getColor(drawingInfo.renderer.symbol.color)};border:${$item.getBorder(drawingInfo.renderer.symbol.outline)};"></div>
-					<span style="padding:0px 5px 0px 5px;display:table-cell;vertical-align:middle;" >${name}</span>
+						<div style="float:left;vertical-align:middle;width:25px;height:25px;background-color:${$item.getColor(drawingInfo.renderer.symbol.color)};border:${$item.getBorder(drawingInfo.renderer.symbol.outline)};"></div>
+						<span style="padding:0px 5px 0px 5px;display:table-cell;vertical-align:middle;" >${name}</span>
 					</div>
 				{{else drawingInfo.renderer.symbol.type=='esriSMS'}}
 					{{if drawingInfo.renderer.symbol.style=="esriSMSCircle"}}
@@ -34,15 +35,15 @@
 						
 						</div>
 					{{else $value.symbol.type=='esriSFS'}}
-						<li style="display:table; margin:10px 5px;" >
+						<li style="margin:10px 5px;" >
 							
-							<div style="display:table-cell;vertical-align:middle;width:25px;height:25px;background:${$item.getColor($value.symbol.color)};border:${$item.getBorder($value.symbol.outline)};" ></div>
+							<div style="float:left;display:table-cell;vertical-align:middle;width:25px;height:25px;background:${$item.getColor($value.symbol.color)};border:${$item.getBorder($value.symbol.outline)};" ></div>
 							<span style="padding:0px 5px 0px 5px;display:table-cell;vertical-align:middle;">${$value.label}</span>
 						</li>
 					{{else $value.symbol.type=='esriSLS'}}
 						<li style="display:table;" >
 							
-							<div style="margin:8px 5px -15px 5px;;width:25px;height:${$value.symbol.width}px;background:${$item.getColor($value.symbol.color)};"></div>
+							<div style="float:left;margin:8px 5px 5px 5px;;width:25px;height:${$value.symbol.width}px;background:${$item.getColor($value.symbol.color)};"></div>
 							<span style="padding:0px 5px 0px 5px;display:table-cell;vertical-align:middle;">${$value.label}</span>
 						</li>
 					{{/if}}
